828fc4ccff909bdea0123613d0eb1ffc89f14ceb
daguilarm <damian.aguilarm@gmail.com>
2019-12-03 18:05:40 +0100

    Updated

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,22 +1,22 @@
 @extends('_layouts.master')
 
 @push('meta')
-    <meta property="og:title" content="About {{ $page->siteName }}" />
+    <meta property="og:title" content="Sobre mi - {{ $page->siteName }}" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ $page->getUrl() }}"/>
-    <meta property="og:description" content="A little bit about {{ $page->siteName }}" />
+    <meta property="og:description" content="Damián Aguilar - Programador y desarrollador web. Programando con Laravel y Vuejs. Graduado en Ingeniería Agroalimentaria y Agroambiental" />
 @endpush
 
 @section('body')
-    <h1>About</h1>
+    <h1>Acerca de mi</h1>
 
-    <img src="/assets/img/about.png"
-        alt="About image"
-        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10">
+    <img src="/assets/img/damian-aguilar.jpeg"
+        alt="Damián Aguilar"
+        class="flex rounded-full h-64 w-64 bg-contain mx-auto md:float-right my-6 md:ml-10"
+    >
 
-    <p class="mb-6">This is where you can give a little more information about yourself or site. If you'd like to change the structure of this page, you can find the file at <code>source/about.blade.php</code></p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam possimus earum harum itaque est!</p>
-
-    <p class="mb-6">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum officia dolorem accusantium veniam quae, possimus, temporibus explicabo magni voluptas. fugit natus deserunt atque veniam!</p>
+    <p>Hola!</p>
+    <p>Me llamo Damián Aguilar y te doy la bienvenida a mi blog sobre php, laravel, vuejs... y programación en general. Me inicié en la programación en el año 2002, empezando a programar con php. Desde entonces otros lenguajes como Java, Javascript o Phyton se han cruzado en mi camino.</p>
+    <p>Los últimos años, he estado trabajado en la Universidad Miguel Hernández como investigador, desarrollando aplicaciones (basadas en Laravel), para la mejora y optimización agraria. Esto me lleva a mi formación académica, ya que aunque siempre he trabajado como programador, soy Graduado en Ingeniería Agroalimentaria, y actualemente, estoy terminando el Doctorado en la Escuela Politénica Superior de Orihuela.</p>
+    <p>Actualmente, trabajo como Freelance desarrollando aplicaciones basadas en Laravel y Vuejs.</p>
 @endsection
--- source/contact.blade.php
+++ source/contact.blade.php
@@ -5,69 +5,21 @@
     <meta property="og:type" content="website" />
     <meta property="og:url" content="{{ $page->getUrl() }}"/>
     <meta property="og:description" content="Get in touch with {{ $page->siteName }}" />
+    <style>
+        .email:before {
+            content: "\006a\0068\006f\006e\0040\0067\006d\0061\0069\006c\002e\0063\006f\006d";
+        }
+    </style>
 @endpush
 
 @section('body')
-<h1>Contact</h1>
+<h1>Contacto</h1>
 
 <p class="mb-8">
-    Static sites are unable to handle form submissions. However, there are third-party services, like Tighten’s <a href="https://fieldgoal.io" title="FieldGoal">FieldGoal</a>, which can accept the form submission, email you the result, and redirect back to a thank you page.
+    Puedes contactar conmigo mediante:
+    <ul>
+        <li>Twitter: <a href="https://twitter.com/daguilarm" target="_black">@daguilarm</a></li>
+        <li>Email: <span class="email">damian.aguilarm@gmail.com</span></li>
+    </ul>
 </p>
-
-<form action="/contact" class="mb-12">
-    <div class="flex flex-wrap mb-6 -mx-3">
-        <div class="w-full md:w-1/2 mb-6 md:mb-0 px-3">
-            <label class="block mb-2 text-grey-darkest text-sm font-semibold" for="contact-name">
-                Name
-            </label>
-
-            <input
-                type="text"
-                id="contact-name"
-                placeholder="Jane Doe"
-                name="name"
-                class="block w-full border shadow rounded-lg outline-none mb-2 p-4"
-                required
-            >
-        </div>
-
-        <div class="w-full px-3 md:w-1/2">
-            <label class="block text-grey-darkest text-sm font-semibold mb-2" for="contact-email">
-                Email Address
-            </label>
-
-            <input
-                type="email"
-                id="contact-email"
-                placeholder="email@domain.com"
-                name="email"
-                class="block w-full border shadow rounded-lg outline-none mb-2 p-4"
-                required
-            >
-        </div>
-    </div>
-
-    <div class="w-full mb-12">
-        <label class="block text-grey-darkest text-sm font-semibold mb-2" for="contact-message">
-            Message
-        </label>
-
-        <textarea
-            id="contact-message"
-            rows="4"
-            name="message"
-            class="block w-full border shadow rounded-lg outline-none appearance-none mb-2 px-4 py-6"
-            placeholder="A lovely message here."
-            required
-        ></textarea>
-    </div>
-
-    <div class="flex justify-end w-full">
-        <input
-            type="submit"
-            value="Submit"
-            class="block bg-blue hover:bg-blue-dark text-white text-sm font-semibold tracking-wide uppercase shadow rounded-lg cursor-pointer px-6 py-3"
-        >
-    </div>
-</form>
 @stop
