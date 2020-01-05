e5bc59a7ac35b6c65795e80cda3805f5a8008a45
daguilarm <damian.aguilarm@gmail.com>
2020-01-05 10:19:54 +0100

    Preload css

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -35,6 +35,8 @@
             //
         @endif --}}
 
+        <link rel="dns-prefetch" href="//fonts.googleapis.com">
+        <link rel="preconnect" href="//fonts.googleapis.com" crossorigin>
         <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,300i,400,400i,700,700i,800,800i" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
