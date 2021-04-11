e10261079e5579a8a74ec0e903acfec5335c0a95
daguilarm <damian.aguilarm@gmail.com>
2021-04-11 12:40:43 +0200

    updated

--- source/_assets/js/components/Search.vue
+++ source/_assets/js/components/Search.vue
@@ -1,7 +1,8 @@
 <template>
     <div class="flex flex-1 justify-end items-center text-right px-4">
         <div
-            class="flex absolute md:relative w-full justify-end left-0 top-0 z-10 mt-7 md:mt-0 px-4 md:px-0"
+            class="absolute md:relative w-full justify-end bg-white left-0 top-0 z-10 mt-7 md:mt-0 px-4 md:px-0"
+            :class="{'hidden md:flex': ! searching}"
         >
             <label for="search" class="hidden">Search</label>
 
@@ -9,7 +10,7 @@
                 id="search"
                 v-model="query"
                 ref="search"
-                class="transition-fast relative block h-10 w-full mr-12 -ml-4 sm:mr-0 sm:w-2/3 lg:w-1/3 lg:focus:w-3/4 bg-gray-100 border border-gray-500 focus:border-blue-500 sm:focus:border-blue-700 outline-none cursor-pointer text-gray-700 px-4 pb-0 pt-px"
+                class="transition-fast relative block h-10 w-full lg:w-1/2 lg:focus:w-3/4 bg-gray-100 border border-gray-500 focus:border-blue-400 outline-none cursor-pointer text-gray-700 px-4 pb-0 pt-px"
                 :class="{ 'transition-border': query }"
                 autocomplete="off"
                 name="search"
@@ -27,19 +28,19 @@
 
             <transition name="fade">
                 <div v-if="query" class="absolute left-0 right-0 md:inset-auto w-full lg:w-3/4 text-left mb-4 md:mt-10">
-                    <div class="flex flex-col bg-white border border-b-0 -mt-1 border-blue-400 rounded-b-lg shadow-lg mx-4 md:mx-0">
+                    <div class="flex flex-col bg-white border border-b-0 border-t-0 border-blue-400 rounded-b-lg shadow-lg mx-4 md:mx-0">
                         <a
                             v-for="(result, index) in results"
                             class="bg-white hover:bg-blue-100 border-b border-blue-400 text-xl cursor-pointer p-4"
-                            :class="{ 'rounded-b-lg' : (index === results.length - 1) }"
-                            :href="result.link"
-                            :title="result.title"
+                            :class="{ 'rounded-b-lg': (index === results.length - 1) }"
+                            :href="result.item.link"
+                            :title="result.item.title"
                             :key="result.link"
                             @mousedown.prevent
                         >
-                            {{ result.title }}
+                            {{ result.item.title }}
 
-                            <span class="block font-normal text-gray-700 text-sm my-1" v-html="result.snippet"></span>
+                            <span class="block font-normal text-gray-700 text-sm my-1" v-html="result.item.snippet"></span>
                         </a>
 
                         <div
@@ -52,10 +53,20 @@
                 </div>
             </transition>
         </div>
+
+        <button
+            title="Start searching"
+            type="button"
+            class="flex md:hidden bg-gray-100 hover:bg-blue-100 justify-center items-center border border-gray-500 rounded-full focus:outline-none h-10 px-3"
+            @click.prevent="showInput"
+        >
+            <img src="/assets/img/magnifying-glass.svg" alt="search icon" class="h-4 w-4 max-w-none">
+        </button>
     </div>
 </template>
 
 <script>
+import Fuse from 'fuse.js';
 export default {
     data() {
         return {
@@ -83,7 +94,7 @@ export default {
     },
     created() {
         axios('/index.json').then(response => {
-            this.fuse = new fuse(response.data, {
+            this.fuse = new Fuse(response.data, {
                 minMatchCharLength: 6,
                 keys: ['title', 'snippet', 'categories'],
             });
@@ -93,27 +104,27 @@ export default {
 </script>
 
 <style>
-    input[name='search'] {
-        background-image: url('/assets/img/magnifying-glass.svg');
-        background-position: 0.8em;
-        background-repeat: no-repeat;
-        border-radius: 25px;
-        text-indent: 1.2em;
-    }
-    input[name='search'].transition-border {
-        border-bottom-left-radius: 0;
-        border-bottom-right-radius: 0;
-        border-top-left-radius: .5rem;
-        border-top-right-radius: .5rem;
-    }
-    .fade-enter-active {
-        transition: opacity .5s;
-    }
-    .fade-leave-active {
-        transition: opacity 0s;
-    }
-    .fade-enter,
-    .fade-leave-to {
-        opacity: 0;
-    }
+input[name='search'] {
+    background-image: url('/assets/img/magnifying-glass.svg');
+    background-position: 0.8em;
+    background-repeat: no-repeat;
+    border-radius: 25px;
+    text-indent: 1.2em;
+}
+input[name='search'].transition-border {
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    border-top-left-radius: .5rem;
+    border-top-right-radius: .5rem;
+}
+.fade-enter-active {
+    transition: opacity .5s;
+}
+.fade-leave-active {
+    transition: opacity 0s;
+}
+.fade-enter,
+.fade-leave-to {
+    opacity: 0;
+}
 </style>
