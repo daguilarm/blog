4c31547b3d1f379733a470463f9c4741dbf8973c
daguilarm <damian.aguilarm@gmail.com>
2021-04-17 11:52:51 +0200

    updated

--- source/projects.blade.php
+++ source/projects.blade.php
@@ -15,38 +15,31 @@
     <h1 class="text-blue-800 text-3xl md:text-4xl lg:text-5xl">Proyectos</h1>
     <h2 class="text-xl font-normal">Actualmente estoy desarrollando las siguientes proyectos:</h2>
     <section id="belich" class="mt-8 p-6">
-        <h3 class="text-xl">a) Belich: panel de administración para Laravel.</h3>
+        <h3 class="text-xl">a) Livewire Tables.</h3>
         <p>
-            Belich es un panel de administración desarrollado con la intención de mejorar como programador, por lo tanto no debe utilizarse para producción. Ha sido desarrollado como un <i>package</i> de <a href="https://laravel.com" target="_blank" rel="noopener">Laravel</a>,
-            entre otras funcionalidades, ofrece:
+            Es un componente dinámico de Laravel Livewire para tablas de datos. Es como el famoso datatables basado en Jquery, pero utilizando Livewire, AlpineJS y TailwindCSS.
+        </p>
+        <p>
+            El package está basado en el desarrollado por <a href="https://github.com/rappasoft/laravel-livewire-tables" target="_blank">rappasoft</a>, pero con bastantes mejoras y desarrollos.
         </p>
         <div class="sm:flex">
             <ul class="w-full sm:flex-1">
-                <li>Barra superior o lateral totalmente configurable.</li>
+                <li>Filtros personalizados para cada columna.</li>
                 <li>Descarga de recursos en formato: EXCEL y CSV</li>
-                <li>Gestión nativa de políticas y autorización.</li>
+                <li>Botones de acciones.</li>
+                <li>Paginación de resultados.</li>
                 <li>Completa customización del package.</li>
-                <li>Configuración de botones de acciones.</li>
-                <li>Gestión de breadcrumbs.</li>
-                <li>Dashboard configurable.</li>
-                <li>Tabs, panels, cards, gráficas,...</li>
-                <li>Campos de formulario predeterminados y opción de crear campos propios.</li>
-                <li>Campos de formulario condicionales.</li>
-                <li>Campos relacionales.</li>
-                <li>Busqueda en tiempo real.</li>
                 <li>Y mucho mas...</li>
             </ul>
             <div class="mt-12 w-full sm:flex-1 sm:mt-8">
-                <img src="/assets/img/projects/belich.jpg"
-                    alt="Belich: panel de administración para Laravel"
+                <img src="/assets/img/projects/livewire-tables.png"
+                    alt="Laravel Livewire para tablas de datos"
                     class="rounded shadow-lg"
                 >
             </div>
         </div>
         <div class="w-full mt-12 sm:mt-0">
-            <p><strong>Proyecto en Github:</strong> <a href="https://github.com/daguilarm/belich">https://github.com/daguilarm/belich</a></p>
-            <p><strong>Documentación:</strong> <a href="https://belich.dev">https://belich.dev</a></p>
-            <p><strong>Ejemplo:</strong> <a href="https://github.com/daguilarm/belich-dashboard">https://github.com/daguilarm/belich-dashboard</a></p>
+            <p><strong>Proyecto en Github:</strong> <a href="https://github.com/daguilarm/livewire-tables">https://github.com/daguilarm/livewire-tables</a></p>
         </div>
     </section>
 @endsection
