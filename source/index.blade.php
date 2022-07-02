63e95f2deb37c1651a0460fe794dd165c55aee13
Damian-portatil <damian.aguilarm@gmail.com>
2022-07-02 18:44:23 +0200

    updated

--- source/index.blade.php
+++ source/index.blade.php
@@ -29,7 +29,7 @@
         @endif
     @endforeach
 
-    @foreach ($posts->where('featured', false)->take(6)->chunk(2) as $row)
+    @foreach ($posts->where('featured', false)->take(20)->chunk(2) as $row)
         <div class="flex flex-col md:flex-row md:-mx-6">
             @foreach ($row as $post)
                 <div class="w-full md:w-1/2 md:mx-6">
