13ef0049c0ae801c8f5977a2098cc9be5f0d0c30
Damian-portatil <damian.aguilarm@gmail.com>
2022-07-03 18:11:14 +0200

    fixed

--- source/_components/banner.blade.php
+++ source/_components/banner.blade.php
@@ -9,3 +9,9 @@
         <iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=30&p=48&l=ur1&category=primeday&banner=0CJQFH890BEJ2EREPQR2&f=ifr&linkID=0dee11051e921f2ae16f919c6515de3b&t=ofertiacom-21&tracking_id=ofertiacom-21" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>
     </div>
 @endif
+
+@if(isset($_COOKIE['gdprcookienotice']['detail']['marketing']))
+    <div class="flex justify-center w-full">
+        <iframe src="https://rcm-eu.amazon-adsystem.com/e/cm?o=30&p=48&l=ur1&category=primeday&banner=0CJQFH890BEJ2EREPQR2&f=ifr&linkID=0dee11051e921f2ae16f919c6515de3b&t=ofertiacom-21&tracking_id=ofertiacom-21" width="728" height="90" scrolling="no" border="0" marginwidth="0" style="border:none;" frameborder="0" sandbox="allow-scripts allow-same-origin allow-popups allow-top-navigation-by-user-activation"></iframe>
+    </div>
+@endif
