32b64664614929c630bc19a42ff9511f24dd184d
Damian-portatil <damian.aguilarm@gmail.com>
2022-07-05 18:44:15 +0200

    updated

--- source/about.blade.php
+++ source/about.blade.php
@@ -1,6 +1,6 @@
 ---
 title: Acerca de Damián Aguilar
-description: Información, formación y trayectoria del programador e ingeniero agrícola Damián Aguilar
+description: Información, formación, contacto y trayectoria profesional del doctor en ingeniería agrícola y programador fullstack Damián Aguilar
 ---
 @extends('_layouts.master')
 
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,11 +1,14 @@
 ---
 extends: _layouts.master
 title: El blog de programación de Damián Aguilar
+description: Blog sobre programación de Damián Aguilar, donde se habla sobre todo de PHP, javascript, vuejs, livewire, phyton, java... y mucho más.
 ---
 
 @section('body')
 
-    <h1 class="mt-8 text-2xl font-extrabold text-blue-700">Últimos artículos y guías sobre programación, diseño web, marketing y SEO.</h1>
+    <h1 class="mt-8 text-2xl font-extrabold text-blue-700">
+        Últimos artículos sobre programación, diseño web, marketing y SEO.
+    </h1>
     <h2 class="-mt-1 mb-8 p-2 text-xl text-gray-600 italic border-l-4 bg-neutral-100 text-neutral-600 border-neutral-500">
         En este blog nos gustan principalemente hablar sobre las siguientes tecnologías: PHP, Laravel, Javascript, VueJS, AlpineJS y Angular.
     </h2>
